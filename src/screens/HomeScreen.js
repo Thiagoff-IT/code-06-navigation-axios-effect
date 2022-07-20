281aea655f213e3dc9c1f67dcb6b812e95face90
Alex Kutzke <alex@kutzke.com.br>
2022-07-20 14:46:47 -0300

    Inicializa componente SearchBar

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import { Feather } from '@expo/vector-icons'; 
+import SearchBar from '../components/SearchBar'; 
 
 const HomeScreen = () => {
   return(
     <View>
       <Feather name="search" size={24} color="black" />
+      <SearchBar />
       <Text>Home Screen!!</Text>
     </View>
   )
