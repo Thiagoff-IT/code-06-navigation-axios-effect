faa6be5734a392191b2b6ed7c19d651cecb30108
Alex Kutzke <alex@kutzke.com.br>
2022-07-20 14:52:52 -0300

    Adiciona estilo para SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,35 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, TextInput, StyleSheet} from 'react-native';
+import { Feather } from '@expo/vector-icons'; 
 
 const SearchBar = () => {
   return(
-    <View>
-      <Text>Search bar!!</Text>
+    <View style={styles.container}>
+      <Feather name="search" size={25} color="black" />
+      <TextInput 
+        autoCapitalize="none"
+        autoCorrect={false}
+        placeholder="Search"
+        style={styles.textInput}
+      />
     </View>
   )
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: 'lightgrey',
+    margin: 10,
+    alignItems: 'center',
+  },
+  textInput: {
+    fontSize: 18,
+    flex: 1,
+    marginLeft: 10,
+  }
+});
 
 export default SearchBar;
