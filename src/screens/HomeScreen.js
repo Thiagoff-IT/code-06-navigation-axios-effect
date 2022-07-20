e4037a0cd1ba37d2b352e1e05723dd8e09db982a
Alex Kutzke <alex@kutzke.com.br>
2022-07-20 14:39:45 -0300

    Adiciona Expo Vector Icons (Feather)

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import { Feather } from '@expo/vector-icons'; 
 
 const HomeScreen = () => {
   return(
     <View>
+      <Feather name="search" size={24} color="black" />
       <Text>Home Screen!!</Text>
     </View>
   )
