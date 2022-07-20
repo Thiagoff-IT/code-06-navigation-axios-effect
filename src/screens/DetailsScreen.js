0cce343169f7a4ae370f8125500747d1c3d62af8
Alex Kutzke <alex@kutzke.com.br>
2022-07-20 16:08:09 -0300

    Navegação com parâmetros e requisição

--- src/screens/DetailsScreen.js
+++ src/screens/DetailsScreen.js
@@ -1,11 +1,30 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet } from 'react-native';
+import tmdb from '../api/tmdb';
 
-const DetailsScreen = ({navigation, route}) => {
+const DetailsScreen = ({ navigation, route }) => {
+  const [movie, setMovie] = useState({});
 
+  useEffect(() => {
+    getMovie(route.params.id);
+  }, []);
+  
+  async function getMovie(id) {
+    try {
+      const response = await tmdb.get(`/movie/${id}`, {
+        params: {
+        }
+      })
+      setMovie(response.data);
+    }
+    catch (err) {
+      console.log(err);
+    }
+
+  }
   return (
     <>
-      <Text>{route.params.id}</Text>
+      <Text>{movie.original_title}</Text>
     </>
   )
 }
